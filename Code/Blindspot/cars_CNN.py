2d71af2f44297a7af3283fb996b009c0a9ad0a70
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-05-20 00:52:56 -0700

    Update cars_CNN.py

--- Code/Blindspot/cars_CNN.py
+++ Code/Blindspot/cars_CNN.py
@@ -3,23 +3,25 @@
 Created on Sat May 19 22:20:55 2018
 
 Using a Keras CNN (Convolutional Neural Network) to identify cars in the image versus no cars in the image.
-Version 1.0
+Version 2.0
 
 @author: Etcyl
 """
 
 import build_cnn
+import keras
 from keras.datasets import cifar10
 import random 
 
 set_size = 32
-
+num_classes = 10 #Change this to 2 when y_test and y_train are made separately from the cifar10 labels
 (x_train, y_train), (x_test, y_test) = cifar10.load_data() #Datastructure for all of the images
+
 
 #Convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-        
+
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
@@ -42,11 +44,11 @@ for i in range(set_size):
     img = cv2.resize(img, (32, 32))
     x_test[i] =
     y_test[i] = 
-"""
+
 
 cnn = build_cnn.buildCNN()
 
-"""
+
 cnn.fit(x_train[0:1000], y_train[0:1000], batch_size=batch_size, epochs=epochs,
                   validation_data=(x_test[0:32], y_test[0:32]), shuffle=True)
             scores = cnn.evaluate(x_test[0:32], y_test[0:32], verbose=1)

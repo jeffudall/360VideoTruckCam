0b9b5623bce37ffae8752314796a0d0c83c7405a
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-05-20 01:34:04 -0700

    Update cars_CNN.py

--- Code/Blindspot/cars_CNN.py
+++ Code/Blindspot/cars_CNN.py
@@ -13,11 +13,12 @@ import keras
 from keras.datasets import cifar10
 import random 
 
-set_size = 32
+set_size = 32 #How many images are in the dataset
 num_classes = 10 #Change this to 2 when y_test and y_train are made separately from the cifar10 labels
 (x_train, y_train), (x_test, y_test) = cifar10.load_data() #Datastructure for all of the images
 
 #Convert class vectors to binary class matrices
+#E.G. 3 --> 0011, assuming 4 total labels (cars, no cars, pedestrian, bike, etc.)
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
@@ -29,11 +30,11 @@ x_test /= 255
 #Load and resize all of the images to be (32, 3)
 """
 for i in range(set_size):
-    #Set the training data
-    img_name = str(i) + '.jpg'
-    random_test_val = random.randint(1, 32) #Get a random image to train on 
-    img = cv2.imread(img_name, 3)
-    img = cv2.resize(img, (32, 32))
+    img_name = str(i) + '.jpg' #Current for-loop value of i plus a .jpg label creates the (saved) image name
+    random_test_val = random.randint(1, 32) #Get a random image to train on on the interval [1, 32]
+    img = cv2.imread(img_name, 3) #Read the current image into variable img
+    img = cv2.resize(img, (32, 32)) #Resize to the shape of the expected CNN, in this case 32, 32
+    #Change the old x_train and y_train CIFAR-10 images to the newly created images
     x_train[i] =  
     y_train[i] = 
     

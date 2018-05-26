8549653961c22fb42ccd8af3ba06b0c100828058
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-05-26 16:29:12 -0700

    Removed unnecessary CIFAR-10 dataset
    Started adding a data generator following the syntax style from Rosebrock's page: https://www.pyimagesearch.com/2018/04/16/keras-and-convolutional-neural-networks-cnns/

--- Code/Blindspot/cars_CNN.py
+++ Code/Blindspot/cars_CNN.py
@@ -1,61 +1,54 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat May 19 22:20:55 2018
-
 Using a Keras CNN (Convolutional Neural Network) to identify cars in the image versus no cars in the image.
 Version 2.0
-
 @author: Etcyl
 """
-#from sklearn.model_selection import train_test_split
-#from keras.preprocessing.image import img_to_array
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import img_to_array
 #from sklearn.preprocessing import LabelBinarizer
-from keras.datasets import cifar10
+#from keras.datasets import cifar10
 import keras
 import build_cnn
 import random 
 import cv2
 
+IMAGE_WIDTH = 32
+IMAGE_HEIGHT = 32
 epochs = 5
 batch_size = 32
 set_size = 32
-num_classes = 10 #Change this to 2 when y_test and y_train are made separately from the cifar10 labels
-(x_train, y_train), (x_test, y_test) = cifar10.load_data() #Datastructure for all of the images
+num_classes = 2
 
-#Convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+#Make a list of training data and class labels
+data = []
+labels = []
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+#Get image paths and randomly shuffle them
+imagePaths = sorted(list(paths.list_images(args["dataset"])))
+random.seed(42)
+random.shuffle(imagePaths)
+"""
 
-#Adjust the dataset to be in the category [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#This should be done with LabelBinarizer()
-for i in range(32):
-    y_train[i] = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    y_test[i] = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    
 #Load and resize all of the images to be (32, 3)
-#Use img_to_array() for this after resizing, then append to the training data list
-for i in range(set_size):
-    #Set the training data
-    img_name = str(i) + '.jpg'
-    img = cv2.imread(img_name, 3)
-    img = cv2.resize(img, (32, 32))
-    x_train[i] =  img
+for imagePath in imagePaths:
+    img = cv2.imread(imagePath)
+    img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT))
+    img = img_to_array(img)
+    data.append(img)
     
-    #Set the test data
+    #Set the class label
     random_test_val = random.randint(0, 32) #Get a random image to train on 
     img_name = str(random_test_val) + '.jpg'
-    img = cv2.imread(img_name, 3)
-    img = cv2.resize(img, (32, 32))
-    x_test[i] = img
-
+    #Get the label from the imagePath
+    #label = imagePath.split(os.path.sep)[-2]
+    labels.append(label)
+    
 cnn = build_cnn.buildCNN()
 cnn.fit(x_train[0:set_size], y_train[0:set_size], batch_size=batch_size, epochs=epochs,
         validation_data=(x_test[0:set_size], y_test[0:set_size]), shuffle=True)
 
 scores = cnn.evaluate(x_test[0:set_size], y_test[0:set_size], verbose=1)
 print('Test accuracy:', scores[1])
+"""

efbf576a1a3171e2e74279a757adc7107eded6f6
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-05-26 16:34:38 -0700

    Added binarizer, split data, and augmented data

--- Code/Blindspot/cars_CNN.py
+++ Code/Blindspot/cars_CNN.py
@@ -7,7 +7,7 @@ Version 2.0
 """
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import img_to_array
-#from sklearn.preprocessing import LabelBinarizer
+#rom sklearn.preprocessing import LabelBinarizer
 #from keras.datasets import cifar10
 import keras
 import build_cnn
@@ -19,7 +19,7 @@ IMAGE_HEIGHT = 32
 epochs = 5
 batch_size = 32
 set_size = 32
-num_classes = 2
+num_classes = 2 #Change this to 2 when y_test and y_train are made separately from the cifar10 labels
 
 #Make a list of training data and class labels
 data = []
@@ -31,6 +31,7 @@ random.seed(42)
 random.shuffle(imagePaths)
 """
 
+    
 #Load and resize all of the images to be (32, 3)
 for imagePath in imagePaths:
     img = cv2.imread(imagePath)
@@ -44,7 +45,26 @@ for imagePath in imagePaths:
     #Get the label from the imagePath
     #label = imagePath.split(os.path.sep)[-2]
     labels.append(label)
-    
+
+#Scale the raw pixel intensities to the range [0, 1]
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+
+#Binarize the labels
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+
+#Partition the data into training and testing splits using 80% of
+#the data for training and the remaining 20% for testing
+(trainX, testX, trainY, testY) = train_test_split(data,
+    labels, test_size = 0.2, random_state = 42)
+
+#Create the image generator for data augmentation
+aug = ImageDataGenerator(rotation_range = 25, width_shift_range = 0.1,
+    height_shift_range = 0.1, shear_range = 0.2, zoom_range = 0.2,
+    horizontal_flip = True, fill_mode = "nearest")
+
+
 cnn = build_cnn.buildCNN()
 cnn.fit(x_train[0:set_size], y_train[0:set_size], batch_size=batch_size, epochs=epochs,
         validation_data=(x_test[0:set_size], y_test[0:set_size]), shuffle=True)

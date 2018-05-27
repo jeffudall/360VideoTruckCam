7c889b6e7bb21c2a84e4e56c721587a1188284fa
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-05-27 13:03:33 -0700

    Create extended_classify.py

---
+++ Code/Blindspot/extended_classify.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 26 23:07:48 2018
+
+Extends the classify.py program to iterate over a "classify / example" directory,
+instead of asking the model to classify one image at a time.
+Assumes that the classifier is already trained and serialized.
+
+Modified from source:
+https://www.pyimagesearch.com/2018/04/16/keras-and-convolutional-neural-networks-cnns/
+@author: Etcyl
+"""
+
+# import the necessary packages
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import numpy as np
+import imutils
+import pickle
+import cv2
+
+# load the trained convolutional neural network and the label
+# binarizer
+print("[INFO] loading network...")
+model = load_model("model")
+lb = pickle.loads(open("labelbin", "rb").read())
+#BEGIN FOR-LOOP
+#Load directory images
+# initialize the data and labels
+data = []
+labels = []
+ 
+# grab the image paths and randomly shuffle them
+print("[INFO] loading images...")
+imagePaths = sorted(list(paths.list_images("verification"))) #"verification" directory contains folders for class testing data
+random.seed(42)
+random.shuffle(imagePaths)
+#Pre-process for classification
+#Classify the current image in loop
+#Display the image, wait 500 ms or 1 sec
+#END FOR-LOOP
+
+"""
+# load the image
+image = cv2.imread("image1.jpg")
+output = image.copy()
+ 
+# pre-process the image for classification
+image = cv2.resize(image, (96, 96))
+image = image.astype("float") / 255.0
+image = img_to_array(image)
+image = np.expand_dims(image, axis=0)
+
+# load the trained convolutional neural network and the label
+# binarizer
+print("[INFO] loading network...")
+model = load_model("model")
+lb = pickle.loads(open("labelbin", "rb").read())
+ 
+# classify the input image
+print("[INFO] classifying image...")
+proba = model.predict(image)[0]
+idx = np.argmax(proba)
+label = lb.classes_[idx]
+
+# we'll mark our prediction as "correct" of the input image filename
+# contains the predicted label text (obviously this makes the
+# assumption that you have named your testing image files this way)
+filename = "truck"
+correct = "correct" if filename.rfind(label) != -1 else "incorrect"
+ 
+# build the label and draw the label on the image
+label = "{}: {:.2f}% ({})".format(label, proba[idx] * 100, correct)
+output = imutils.resize(output, width=400)
+cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,
+	0.7, (0, 255, 0), 2)
+ 
+# show the output image
+print("[INFO] {}".format(label))
+cv2.imshow("Output", output)
+cv2.waitKey(0)
+"""
